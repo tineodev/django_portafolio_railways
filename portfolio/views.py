04726d74dd7efa9e4be0df6fdcd22ffb19c11215
tineodev <tineo.dev@gmail.com>
2022-12-07 09:47:14 -0500

    Add index URLS

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import View
+
+class Index(View):
+    def get(self, request):
+        template_name = 'portfolio/index.html'
+        return render(request, template_name)
