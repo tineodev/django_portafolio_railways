64b577ba6fd7de430a238b72e10d1b18947a6719
tineodev <tineo.dev@gmail.com>
2022-12-09 15:41:53 -0500

    Config DB

--- zproject/settings.py
+++ zproject/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'zproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'pImgHL856rTRPb70usGP',
+        'HOST': 'containers-us-west-151.railway.app',
+        'PORT': '5552',
     }
 }
 
